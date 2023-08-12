0cdd55ae6ac40b22eb78f504534025b2b3592ec5
noizzer28 <colosova28@yandex.ru>
2023-08-13 01:42:43 +0300

    разбивка на компоненты, добавление изображений

---
+++ src/components/sidebar.js
@@ -0,0 +1,43 @@
+export  const SideBar = () => {
+    return(          <div className="main__sidebar sidebar">
+    <div className="sidebar__personal">
+      <p className="sidebar__personal-name">Sergey.Ivanov</p>
+      <div className="sidebar__icon">
+        <svg alt="logout">
+          <use xlinkHref="./icons/sprite.svg#logout"></use>
+        </svg>
+      </div>
+    </div>
+    <div className="sidebar__block">
+      <div className="sidebar__list">
+        <div className="sidebar__item">
+          <a className="sidebar__link" href="#">
+            <img
+              className="sidebar__img"
+              src="./playlist01.png"
+              alt="day's playlist"
+            />
+          </a>
+        </div>
+        <div className="sidebar__item">
+          <a className="sidebar__link" href="#">
+            <img
+              className="sidebar__img"
+              src="./playlist02.png"
+              alt="day's playlist"
+            />
+          </a>
+        </div>
+        <div className="sidebar__item">
+          <a className="sidebar__link" href="#">
+            <img
+              className="sidebar__img"
+              src="./playlist03.png"
+              alt="day's playlist"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>)
+  }
