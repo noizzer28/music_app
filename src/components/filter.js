7943935630b3280a0c3659d7e92681f8edc080fc
noizzer28 <colosova28@yandex.ru>
2023-08-20 19:05:29 +0300

    создание dropdown списков по году и жанру

--- src/components/filter.js
+++ src/components/filter.js
@@ -8,20 +8,40 @@ class PlaylistFilter extends React.Component {
     constructor () {
         super();
         this.state = {
-            isActive: false
+            isActiveAuthor: false,
+            isActiveYear: false,
+            isActiveGenre: false
         }
     }
-    handler = () => {
+    handlerAuthor = () => {
         console.log('click')
         this.setState({
-            isActive: !this.state.isActive
+            isActiveAuthor: !this.state.isActiveAuthor,
+            isActiveYear: false,
+            isActiveGenre: false,
+        })
+    }
+    handlerYear = () => {
+        console.log("click")
+        this.setState({
+            isActiveAuthor: false,
+            isActiveYear: !this.state.isActiveYear,
+            isActiveGenre: false,
+        })
+    }
+    handlerGenre = () => {
+        console.log("click")
+        this.setState({
+            isActiveAuthor: false,
+            isActiveYear: false,
+            isActiveGenre: !this.state.isActiveGenre,
         })
     }
 
     renderAuthors = () => {
         let AuthorList = []
         AuthorList = Tracks.map((track) => (
-            <li key={track.id} className="author__list">
+            <li key={track.id} className="filter__list">
                 {track.author}
             </li>
         ));
@@ -29,22 +49,44 @@ class PlaylistFilter extends React.Component {
 
     }
 
+    renderYears = () => {
+        let Years = ["1990-2000", "2001-2010", "2011-2020", "2020+"]
+        let YearsList = []
+        YearsList = Years.map((year, index) => {
+            return (<li key={index} className="filter__list">
+                {year}
+            </li>)
+        });
+        return YearsList
+    }
+
+    renderGenre = () => {
+        let Genres = ["Rock", "Pop", "RnB", "Jazz", "Hip-Hop"]
+        let GenreList = []
+        GenreList = Genres.map((genre, index) => {
+            return (<li key={index} className="filter__list">
+            {genre}
+        </li>)
+        })
+        return GenreList
+    }
+
     render() {
 
         return <div className="centerblock__filter filter">
         <div className="filter__title">Искать по:</div>
-        <div className="filter__button button-author _btn-text" onClick={this.handler} >исполнителю</div>
+        <div className="filter__button button-author _btn-text" onClick={this.handlerAuthor} >исполнителю</div>
         <div className="filter__author">
-        {this.state.isActive ? <ul className="filter__modal">{this.renderAuthors()}</ul> : ''}
+        {this.state.isActiveAuthor ? <ul className="filter__modal">{this.renderAuthors()}</ul> : ''}
         </div>
-        <div className="filter__button button-year _btn-text">году выпуска</div>
-        <div className="filter__button button-genre _btn-text">жанру</div>
+        <div className="filter__button button-year _btn-text" onClick={this.handlerYear}>году выпуска</div>
+        {this.state.isActiveYear ? <ul className="filter__modal">{this.renderYears()}</ul> : ''}
+        <div className="filter__button button-genre _btn-text" onClick={this.handlerGenre}>жанру</div>
+        {this.state.isActiveGenre ? <ul className="filter__modal">{this.renderGenre()}</ul> : ''}
+        
     </div>
     }
 }
 
-// const PlaylistFilter = () => {
-//     return ()
-//   }
 
 export default PlaylistFilter
