e7cba9416c960575230fa6ca5300117127b17a18
noizzer28 <colosova28@yandex.ru>
2023-08-23 17:43:12 +0300

    переписан код на функциональный компонент

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,89 +1,74 @@
-import { Tracks } from "./songs"
-import React from "react";
+import React, { useState } from "react";
 import SimpleBar from 'simplebar-react';
 import 'simplebar-react/dist/simplebar.min.css';
+import { Tracks } from "./songs";
 
+const PlaylistFilter = () => {
+    const [isActiveAuthor, setIsActiveAuthor] = useState(false);
+    const [isActiveYear, setIsActiveYear] = useState(false);
+    const [isActiveGenre, setIsActiveGenre] = useState(false);
 
+    const handleAuthor = () => {
+        setIsActiveAuthor(!isActiveAuthor);
+        setIsActiveYear(false);
+        setIsActiveGenre(false);
+    };
 
-class PlaylistFilter extends React.Component {
+    const handleYear = () => {
+        setIsActiveAuthor(false);
+        setIsActiveYear(!isActiveYear);
+        setIsActiveGenre(false);
+    };
 
-    constructor () {
-        super();
-        this.state = {
-            isActiveAuthor: false,
-            isActiveYear: false,
-            isActiveGenre: false
-        }
-    }
-    handlerAuthor = () => {
-        this.setState({
-            isActiveAuthor: !this.state.isActiveAuthor,
-            isActiveYear: false,
-            isActiveGenre: false,
-        })
-    }
-    handlerYear = () => {
-        this.setState({
-            isActiveAuthor: false,
-            isActiveYear: !this.state.isActiveYear,
-            isActiveGenre: false,
-        })
-    }
-    handlerGenre = () => {
-        this.setState({
-            isActiveAuthor: false,
-            isActiveYear: false,
-            isActiveGenre: !this.state.isActiveGenre,
-        })
-    }
+    const handleGenre = () => {
+        setIsActiveAuthor(false);
+        setIsActiveYear(false);
+        setIsActiveGenre(!isActiveGenre);
+    };
 
-    renderAuthors = () => {
-        let AuthorList = []
-        AuthorList = Tracks.map((track) => (
+    const renderAuthors = () => {
+        return Tracks.map((track) => (
             <li key={track.id} className="filter__list">
                 {track.author}
             </li>
         ));
-        return AuthorList;
+    };
 
-    }
-
-    renderYears = () => {
-        let Years = ["1990-2000", "2001-2010", "2011-2020", "2020+"]
-        let YearsList = []
-        YearsList = Years.map((year, index) => {
-            return (<li key={index} className="filter__list">
+    const renderYears = () => {
+        const years = ["1990-2000", "2001-2010", "2011-2020", "2020+"];
+        return years.map((year, index) => (
+            <li key={index} className="filter__list">
                 {year}
-            </li>)
-        });
-        return YearsList
-    }
+            </li>
+        ));
+    };
 
-    renderGenre = () => {
-        let Genres = ["Rock", "Pop", "RnB", "Jazz", "Hip-Hop"]
-        let GenreList = []
-        GenreList = Genres.map((genre, index) => {
-            return (<li key={index} className="filter__list">
-            {genre}
-        </li>)
-        })
-        return GenreList
-    }
+    const renderGenre = () => {
+        const genres = ["Rock", "Pop", "RnB", "Jazz", "Hip-Hop"];
+        return genres.map((genre, index) => (
+            <li key={index} className="filter__list">
+                {genre}
+            </li>
+        ));
+    };
 
-    render() {
+    return (
+        <div className="centerblock__filter filter">
+            <div className="filter__title">Искать по:</div>
+            <div className="filter__button button-author _btn-text" onClick={handleAuthor}>исполнителю</div>
+            {isActiveAuthor && (
+                <ul className="filter__modal filter__author">
+                    <SimpleBar forceVisible="y" style={{ height: '300px' }}>
+                        {renderAuthors()}
+                    </SimpleBar>
+                </ul>
+            )}
+            <div className="filter__button button-year _btn-text" onClick={handleYear}>году выпуска</div>
+            {isActiveYear && <ul className="filter__modal filter__year">{renderYears()}</ul>}
+            <div className="filter__button button-genre _btn-text" onClick={handleGenre}>жанру</div>
+            {isActiveGenre && <ul className="filter__modal filter__genre">{renderGenre()}</ul>}
+        </div>
+    );
+};
 
-        return <div className="centerblock__filter filter">
-        <div className="filter__title">Искать по:</div>
-        <div className="filter__button button-author _btn-text" onClick={this.handlerAuthor} >исполнителю</div>
-        {this.state.isActiveAuthor ? <ul className="filter__modal filter__author"><SimpleBar forceVisible="y"    style={{ height: '300px' }}>{this.renderAuthors()}</SimpleBar></ul>: ''}
-        <div className="filter__button button-year _btn-text" onClick={this.handlerYear}>году выпуска</div>
-        {this.state.isActiveYear ? <ul className="filter__modal filter__year">{this.renderYears()}</ul> : ''}
-        <div className="filter__button button-genre _btn-text" onClick={this.handlerGenre}>жанру</div>
-        {this.state.isActiveGenre ? <ul className="filter__modal filter__genre">{this.renderGenre()}</ul> : ''}
-        
-    </div>
-    }
-}
-
-
-export default PlaylistFilter
+export default PlaylistFilter;
