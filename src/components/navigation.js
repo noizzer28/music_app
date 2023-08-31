66d395bba2c5bed8599cc60984fdc1336e68c487
noizzer28 <colosova28@yandex.ru>
2023-09-01 00:45:24 +0300

    навигация переписана на функциональный компонент

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,45 +1,45 @@
-import React from "react"
+import React, { useState } from "react";
 
-class Nav extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state =  {
-      isActive: true,
-    }
-    this.hideSideBar  = this.hideSideBar.bind(this)
-  }
-  render() {
-    return(          <nav className="main__nav nav">
-    <div className="nav__logo logo">
-      <img className="logo__image" src="./logo.png" alt="logo" />
-    </div>
-    <div className="nav__burger burger" onClick={this.hideSideBar}>
-      <span className="burger__line"></span>
-      <span className="burger__line"></span>
-      <span className="burger__line"></span>
-    </div>
-    {this.state.isActive ? "" :     <div className="nav__menu menu">
-      <ul className="menu__list">
-        <li className="menu__item">
-          <a href="#" className="menu__link">Главное</a>
-        </li>
-        <li className="menu__item">
-          <a href="#" className="menu__link">Мой плейлист</a>
-        </li>
-        <li className="menu__item">
-          <a href="../signin.html" className="menu__link">Войти</a>
-        </li>
-      </ul>
-    </div> }
+function Nav() {
+  const [isActive, setIsActive] = useState(true);
 
-  </nav>)}
+  const hideSideBar = () => {
+    setIsActive(!isActive);
+  };
 
-  hideSideBar () {
-    this.setState({isActive: !this.state.isActive})
-    console.log("clicked")
-  }
-  }
-   
+  return (
+    <nav className="main__nav nav">
+      <div className="nav__logo logo">
+        <img className="logo__image" src="./logo.png" alt="logo" />
+      </div>
+      <div className="nav__burger burger" onClick={hideSideBar}>
+        <span className="burger__line"></span>
+        <span className="burger__line"></span>
+        <span className="burger__line"></span>
+      </div>
+      {!isActive && (
+        <div className="nav__menu menu">
+          <ul className="menu__list">
+            <li className="menu__item">
+              <a href="#" className="menu__link">
+                Главное
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="#" className="menu__link">
+                Мой плейлист
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="../signin.html" className="menu__link">
+                Войти
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+}
 
-
-export default Nav
+export default Nav;
