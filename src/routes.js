511d90d5b59f240ee10ea8718a5b7500ac147512
noizzer28 <colosova28@yandex.ru>
2023-09-25 23:28:56 +0300

    авторизация по логину и паролю, запрос на сервер

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import MainApp from "./pages/main/main";
-import { Login } from "./pages/login/login";
+import  { AuthPage } from "./pages/authorisation/login";
 import { Favorites } from "./pages/favorites/favorites";
-import { Authorisation } from "./pages/authorisation/auth";
 import { Category } from "./pages/category/category";
 import { NotFound } from "./pages/not-found/not-found";
 import { ProtectedRoute } from "./components/protected-routes/protected";
@@ -11,13 +10,19 @@ export const AppRoutes = ({ setToken }) => {
 
     return (
         <Routes>
-            <Route element={<ProtectedRoute  isAllowed={Boolean(localStorage.getItem("token"))}></ProtectedRoute>}>
+            <Route element={<ProtectedRoute  isAllowed={Boolean(localStorage.getItem('token'))}></ProtectedRoute>}>
                 <Route path="/" element={<MainApp setToken={setToken}/>}/>
                 <Route path="/favorites" element={<Favorites/>}/>
                 <Route path="/category/:id" element={<Category/>}/>
             </Route>
-            <Route path="/login" element={<Login setToken={setToken}/>}/>
-            <Route path="/authorisation" element={<Authorisation/>}/>   
+            <Route
+                path="/login"
+                element={<AuthPage setToken={setToken} isLoginMode={true}></AuthPage>}
+            ></Route>
+            <Route
+                path="/register"
+                element={<AuthPage setToken={setToken} isLoginMode={false}></AuthPage>}
+            ></Route>
             <Route path="*" element={<NotFound/ >} />
 
         </Routes>
